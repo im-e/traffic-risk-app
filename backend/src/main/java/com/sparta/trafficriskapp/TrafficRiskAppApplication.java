6ca956119f9b6fc3c118a41ce056c81bf12113e4
im-e <immie@riseup.net>
2024-06-24 22:54:14 +0100

    updated gitignore

---
+++ backend/src/main/java/com/sparta/trafficriskapp/TrafficRiskAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.trafficriskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficRiskAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficRiskAppApplication.class, args);
+    }
+
+}
