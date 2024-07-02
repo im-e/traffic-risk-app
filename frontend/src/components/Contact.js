e0dea33458f04d00fb09bbd1cae2761dcf7ea771
im-e <immie@riseup.net>
2024-07-02 23:45:18 +0100

    global styling

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact">
             <h1>Contact Us</h1>
             <p>This is the Contact page.</p>
         </div>
